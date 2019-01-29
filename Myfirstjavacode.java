52f0f1585b0b862eebfa6c08484e9ad1a2636534
maheshwari <sairammaheshwari@gmail.com>
2019-01-29 12:35:24 -0600

    first commit done

---
+++ Myfirstjavacode.java
@@ -0,0 +1,10 @@
+
+public class Myfirstjavacode {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World , This is my First JAVA program");
+		System.out.println(" It is successfully executed");
+
+	}
+
+}
